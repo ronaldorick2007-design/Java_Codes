97f4f4cf41a9e79c7241837ae252c493991b242f
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-02-07 22:24:12 +0530

    Added prgromme for number triangle

---
+++ Pattern_Generation/NumberTriangle.java
@@ -0,0 +1,32 @@
+// Jave Programme to print numbers in triangular pattern
+// LOGIC:
+//     Nested loops are used, inner loop is used to print inline pattern
+//     outer loops is to move to next lines.
+// COMPLEXITY:
+//     Time : O(N^2)   Nested loop
+//     Space : O(1)    only i, j, num
+
+//Importing packages required for input
+import java.util.Scanner;
+
+public class NumberTriangle{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+
+        //Getting input from the user
+        System.out.print("Enter no of rows: ");
+        int rows = scan.nextInt();
+
+        //Outer loop
+        for(int i = 1; i <= rows; i++){
+            //Inner loop
+            for(int j = 1; j <= i; j++){
+                //Printing j for consecutive numbers till i
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        scan.close();
+    }
+}
